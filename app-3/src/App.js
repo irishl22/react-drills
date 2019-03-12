62b36645bd82be954cf5a0b2e7def7e116b7e92a
Lauren Irish <lirish2@gmail.com>
2019-03-12 14:38:28 -0600

    updated with answer

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+      list: ["dogs", "cats", "birds", "cows"]
+    }
+  }
+
+  handleChange(val) {
+    this.setState({ text: val })
+  } 
+
   render() {
+    let newList = this.state.list.filter((element, index) => {
+      return element.includes(this.state.text)
+    })
+    .map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={ (e) => this.handleChange(e.target.value)} type="text" />
+       {newList}
       </div>
     );
   }
